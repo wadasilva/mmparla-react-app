18d5f327bc17671507ca1413369db6f77e935ec0
Walter Alves <wadasilvajunior@gmail.com>
2022-05-22 16:12:36 +0200

    - Adds AOS library for animations.

--- web/src/components/site/Site.jsx
+++ web/src/components/site/Site.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import HeroBlock from './HeroBlock';
-import AboutBlock from './AboutBlock';
-import ServiceBlock from './ServiceBlock';
-import WorkBlock from './WorkBlock';
-import BrandBlock from './BrandBlock';
-import TestimonialsBlock from './TestimonialsBlock';
-import ContactBlock from './ContactBlock';
+import React, { useEffect } from "react";
+import HeroBlock from "./HeroBlock";
+import AboutBlock from "./AboutBlock";
+import ServiceBlock from "./ServiceBlock";
+import WorkBlock from "./WorkBlock";
+import BrandBlock from "./BrandBlock";
+import TestimonialsBlock from "./TestimonialsBlock";
+import ContactBlock from "./ContactBlock";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Site = () => {
-    return (
-        <React.Fragment>
-            <HeroBlock />
-            <AboutBlock />
-            <ServiceBlock />
-            <WorkBlock />
-            <BrandBlock />
-            <TestimonialsBlock />
-            <ContactBlock />
-        </React.Fragment>
-    );
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <HeroBlock />
+      <AboutBlock />
+      <ServiceBlock />
+      <WorkBlock />
+      <BrandBlock />
+      <TestimonialsBlock />
+      <ContactBlock />
+    </React.Fragment>
+  );
 };
 
 export default Site;
