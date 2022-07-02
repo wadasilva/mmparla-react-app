41dcd504d1b718fca3737a5876e9aa8f09134166
Walter Alves <wadasilvajunior@gmail.com>
2022-07-02 23:20:11 +0200

    Enables trust proxy in index.js file and fixes API redirect loop

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
-const config = require("config-secrets");
+require('./startup/dotenv'); //Should load as earlier as possible before app boot up
 require("./startup/folders")();
+const config = require("config");
 const starttupDebugger = require("debug")("app:startup");
 const express = require("express");
 const app = express();
@@ -25,11 +26,14 @@ if (!config.get("jwtPrivateKey")) {
 app.use(morgan("combined"));
 morgan(app, { logAllReqHeader: true, maxBodyLength: 5000 });
 
+//Enables trust proxy
+app.enable('trust proxy');
+
 const port = process.env.PORT || 3000;
 const httpsPort = process.env.HTTPS_PORT || 3443;
 app.use(httpsRedirect(httpsPort));
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
   require("./startup/prod")(app);
 }
 
@@ -39,18 +43,17 @@ require("./startup/db")();
 let options = {};
 try {
   options = {
-    key: fs.readFileSync("/run/secrets/api_selfsigned.key", { encoding: "utf-8" }),
-    cert: fs.readFileSync("/run/secrets/api_selfsigned.crt", { encoding: "utf-8" }),
+    key: fs.readFileSync(`/etc/letsencrypt/live/${config.get('domain')}/privkey.pem`, { encoding: "utf-8" }),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/${config.get('domain')}/fullchain.pem`, { encoding: "utf-8" }),
     enableTrace: false,
   };
 } catch (err) {
   sentryLogger.log(`FATAL ERROR: FATAL ERROR: could not config ssl for the app. ${JSON.stringify(err)}`);
   logger.error(`FATAL ERROR: FATAL ERROR: could not config ssl for the app. ${JSON.stringify(err)}`);
 
-  if (process.env.NODE_ENV === 'production')
+  if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
     process.exit(1);
 }
-
 
 httpServer = http.createServer(app).listen(port, "0.0.0.0", true, () => logger.info(`Listening on port ${port}...`));
 httpsServer = https.createServer(options, app).listen(httpsPort, "0.0.0.0", true, () => logger.info(`Listening on https port ${httpsPort}...`));
--- api/middleware/httpsRedirect.js
+++ api/middleware/httpsRedirect.js
@@ -2,16 +2,20 @@ const { logger, sentryLogger } = require("../startup/logging");
 
 module.exports = function(httpsPort) {
   return function (req, res, next) {
-    if (process.env.NODE_ENV === "production") {
-      if (req.headers["x-forwarded-proto"] !== "https") {
-        let url = `https://${req.headers.host}${req.url}`;
-        const port = req.headers.host.split(':')[1];
-        url = url.replace(port, httpsPort);
-        logger.info(url);
-        return res.redirect(url);
+    try {
+      if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+        if (!req.connection.encrypted) {
+          let url = `https://${req.headers.host}${req.url}`;
+          const port = req.headers.host.split(':')[1];
+          url = url.replace(port, httpsPort);
+          return res.redirect(url);
+        }          
       }
-        
+      return next();
+    } catch (error) {
+      logger.error(JSON.stringify(error));
+      sentryLogger.log(error);
+      return res.status(500).send('Internal server error');
     }
-    return next();
   }
 };
